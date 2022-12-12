c31ef2e07891b2c9769f3197adadc89524351596
Afrohead01 <subhamdeb409@gmail.com>
2022-12-12 19:14:31 +0530

    Sequelize

--- 03-rendering-the-product-detail-view/util/database.js
+++ 03-rendering-the-product-detail-view/util/database.js
@@ -1,9 +1,4 @@
-const mysql = require('mysql2')
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'pika.123'
-});
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('node-complete', 'root', 'pika.123', {dialect: 'mysql', host: 'localhost'})
 
-module.exports = pool.promise()
+module.exports = sequelize
